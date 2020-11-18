9bbba4bd8f9964973f03b3d71f523ab6e8075a7f
Richard Lo Sinno <richard.losinno@gmail.com>
2020-11-18 21:07:55 +0100

    update location code

--- SignalRChat/wwwroot/js/chat.js
+++ SignalRChat/wwwroot/js/chat.js
@@ -1,31 +1,16 @@
 ﻿"use strict";
+
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
 
-connection.on("ReceiveMessage", function (user, message) {
+connection.on("ReceiveMessage", function (user, message,location) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    getLocation();
     var li = document.createElement("li");
    
-    var encodedMsg =  user + " says " + msg + " | Sent from ";
+    var encodedMsg =  user + " says " + msg + " | Sent from " + location;
     li.textContent = encodedMsg;
-    
-
-    function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-        } else {
-            li.textContent += "Geolocation is not supported by this browser.";
-        }
-    }
-
-    function showPosition(position) {
-        
-        li.textContent += "Lat: " + position.coords.latitude +
-            " Lon: " + position.coords.longitude;
-    }
 
     document.getElementById("messagesList").appendChild(li);
 });
@@ -37,12 +22,32 @@ connection.start().then(function () {
     return console.error(err.toString());
 });
 
-document.getElementById("sendButton").addEventListener("click", function (event) {
+document.getElementById("sendButton").addEventListener("click", async function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+    var location = await getLocation();
+
+    connection.invoke("SendMessage", user, message,location).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
 });
 
+async function getLocation() {
+    if (navigator.geolocation) {
+
+        async function getCoordinates() {
+            return new Promise(function (resolve, reject) {
+                navigator.geolocation.getCurrentPosition(resolve, reject);
+            });
+        }
+
+        const position = await Promise.resolve(getCoordinates());
+        console.log(position);
+        let loc = "Lat: " + position.coords.latitude + " Lon: " + position.coords.longitude;
+        console.log(loc);
+        return loc;
+    } else {
+        return "Geolocation is not supported by this browser.";
+    }
+}
